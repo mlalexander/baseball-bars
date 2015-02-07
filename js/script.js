6b7eaad68b305bbfd20654d651c7a0f18de29124
Madi Alexander <madi.alexander@gmail.com>
2015-02-07 10:17:18 -0600

    updates for new assignment

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
+
+
+var playerData;
+
 $(document).ready(function() {
-    console.log("Hello world.")
+    getData();
 });
 
 
+function getData() {
+  $.getJSON("js/carlosgomez.json", function(data) {
+    playerData = data;
+    drawChart();
+  });
+}
 
 
+  function drawChart() {
+
+    $(".name").html(playerData.name);
+
+    $.each(playerData.stats, function(i, item) {
+      var width = item.HR * 10;
 
 
+    $(".chart").append(
+        "<div>"+
+          "<div class='year'>"+item.year+"</div>"+"<div class='bar' style='width: "+width+"px'></div>"+"<div class='total'>"+item.HR+"</div>"+
+        "</div>"
+      );
+    });
+
+  }
